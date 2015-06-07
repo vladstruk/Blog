a78a50e41dd7e918affe312e96f3737cc2c249ba
Vlad Struk <vlad.struk1983@gmail.com>
2015-06-07 15:02:30 +0300

    Ability to edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,13 +10,29 @@ class ArticlesController < ApplicationController
 	def create
 	  @article =  Article.new(articles_params)
 	  if @article.save
-	  	flash[:notice] = "New article added"
+	  	flash[:notice] = "New article added."
 	  	redirect_to articles_path
 	  else
-	  	flash[:error] = "None of article fields can be blank"
+	  	flash[:error] = "None of article fields can be blank!"
 	  	render :new
 	  end
 	end
+
+	def edit
+	  @article = Article.find_by_id(params[:id])
+	end
+
+	def update
+	  @article = Article.find_by_id(params[:id])
+	  if @article.update_attributes(articles_params)
+	  	flash[:notice] = "Article #{@article.title} has been just edited."
+	  	redirect_to articles_path
+	  else
+	  	flash[:error] = "None of article fields can be blank!"
+	  	render :edit
+	  end
+	end
+
 
 	private
 
