a5514a67cc899f995739040a2ff9b85df00955e9
Vlad Struk <vlad.struk1983@gmail.com>
2015-06-08 16:44:09 +0300

    Adding tests on sort by fields method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,12 @@ module ApplicationHelper
 
 	def sort_by_field field
       if field == params[:field]
-	    params[:direction] == 'asc' ? params[:direction] = 'desc' : params[:direction] = 'asc'
-	    arrow = {'asc' => '&#8593;', 'desc' => '&#8595;', nil => '&#8645;'}[params[:direction]]
-	    link_to "#{field} #{arrow}".html_safe, articles_path(field: field, direction: params[:direction])
+      	#direction = { 'asc' => 'desc', 'desc' => 'asc', nil => 'asc' }[params[:direction]]
+        params[:direction] == 'asc' ? direction = 'desc' : direction = 'asc'
+	    arrow = {'asc' => '&#8593;', 'desc' => '&#8595;'}[direction]
+	    link_to "#{field} #{arrow}".html_safe, articles_path(field: field, direction: direction)
 	  else
-	  	link_to "#{field} #{'&#8645;'}".html_safe, articles_path(field: field, direction: params[:direction])
+	  	link_to "#{field} #{'&#8645;'}".html_safe, articles_path(field: field, direction: 'asc')
 	  end
 	end
 end
--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -5,33 +5,37 @@ describe "Articles" do
       visit articles_path
 	end
 
-    #index
-	it "should have title Articles", js: true do
-	  expect(page).to have_content("Create new article")
-	end
+    describe "action index"  do   
+ 
+	  it "should have title Articles", js: true do
+	    expect(page).to have_content("Create new article")
+	  end
+    end
 
-    #new
-	it "should have content new article added if article data is valid", js: true do
-	  fill_in "Title", with: "Title"
-	  fill_in "Content", with: "Content"
-	  click_button("Create Article")
-	  expect(page).to have_content("New article added")
-	end
+    describe "action create" do
 
-	it "should have have error message if article data is invalid", js: true do
-	  fill_in "Content", with: "Content"
-	  click_button("Create Article")
-	  expect(page).to have_content("None of article fields can be blank")
-	end
-
-	it "should have have error message if article data is invalid", js: true do
-	  fill_in "Title", with: "Title"
-	  click_button("Create Article")
-	  expect(page).to have_content("None of article fields can be blank")
-	end
+	  it "should have content new article added if article data is valid", js: true do
+	    fill_in "Title", with: "Title"
+	    fill_in "Content", with: "Content"
+	    click_button("Create Article")
+	    expect(page).to have_content("New article added")
+	  end
+  
+	  it "should have have error message if article data is invalid", js: true do
+	    fill_in "Content", with: "Content"
+	    click_button("Create Article")
+	    expect(page).to have_content("None of article fields can be blank")
+	  end
+  
+	  it "should have have error message if article data is invalid", js: true do
+	    fill_in "Title", with: "Title"
+	    click_button("Create Article")
+	    expect(page).to have_content("None of article fields can be blank")
+	  end
+    end
 
     
-    context "after editing" do
+    context "" do
 
 	  let!(:article) { Article.create(title: "Title", content: "Content") }
       let!(:article2) { Article.create(title: "Title2", content: "Content2") }
@@ -40,56 +44,87 @@ describe "Articles" do
         visit articles_path
 	  end
 
-      #edit
-	  it "should have notice of updating if article data is valid", js: true do
-	  	within "#article_#{article.id}" do
-	  	  click_on 'Edit'
-	  	end
-	    fill_in "Title", with: "Title"
-	    fill_in "Content", with: "Content"
-	    click_button("Update Article")
-	    expect(page).to have_content("Article #{article.title} has been just edited.")
-	  end
-  
-	  it "should have have error message if article data is invalid", js: true do
-	  	within "#article_#{article.id}" do
-	  	  click_on 'Edit'
-	  	end
-	    visit edit_article_path(article)
-		fill_in "Title", with: ""
-	    fill_in "Content", with: "Content"
-	    click_button("Update Article")
-	    expect(page).to have_content("None of article fields can be blank")
-	  end
-  
-	  it "should have have error message if article data is invalid", js: true do
-	  	within "#article_#{article.id}" do
-	  	  click_on 'Edit'
-	  	end
-	    visit edit_article_path(article)
-	    fill_in "Title", with: "Title"
-	    fill_in "Content", with: ""
-	    click_button("Update Article")
-	    expect(page).to have_content("None of article fields can be blank")
-	  end
-      
-      #delete
-      it "should have notice of deleting article", js: true do
-	  	within "#article_#{article.id}" do
-	  	  click_on 'Delete'
-	  	end
-	    expect(page).to have_content("Article #{article.title} has been just deleted.")
+      describe "action update" do
+
+	    it "should have notice of updating if article data is valid", js: true do
+	    	within "#article_#{article.id}" do
+	    	  click_on 'Edit'
+	    	end
+	      fill_in "Title", with: "Title"
+	      fill_in "Content", with: "Content"
+	      click_button("Update Article")
+	      expect(page).to have_content("Article #{article.title} has been just edited.")
+	    end
+    
+	    it "should have have error message if article data is invalid", js: true do
+	    	within "#article_#{article.id}" do
+	    	  click_on 'Edit'
+	    	end
+	      visit edit_article_path(article)
+		  fill_in "Title", with: ""
+	      fill_in "Content", with: "Content"
+	      click_button("Update Article")
+	      expect(page).to have_content("None of article fields can be blank")
+	    end
+    
+	    it "should have have error message if article data is invalid", js: true do
+	    	within "#article_#{article.id}" do
+	    	  click_on 'Edit'
+	    	end
+	      visit edit_article_path(article)
+	      fill_in "Title", with: "Title"
+	      fill_in "Content", with: ""
+	      click_button("Update Article")
+	      expect(page).to have_content("None of article fields can be blank")
+	    end
 	  end
       
-      #show
-	  it "should have all article attributes content", js: true do
-	  	within "#article_#{article.id}" do
-	  	  click_on article.title
-	  	end
-	    expect(page).to have_content(article.id)
-	    expect(page).to have_content(article.title)
-	    expect(page).to have_content(article.content)
+      describe "action destroy" do
+
+        it "should have notice of deleting article", js: true do
+	    	within "#article_#{article.id}" do
+	    	  click_on 'Delete'
+	    	end
+	      expect(page).to have_content("Article #{article.title} has been just deleted.")
+	    end
 	  end
+      
+      describe "action show" do
+
+	    it "should have all article attributes content", js: true do
+	    	within "#article_#{article.id}" do
+	    	  click_on article.title
+	    	end
+	      expect(page).to have_content(article.id)
+	      expect(page).to have_content(article.title)
+	      expect(page).to have_content(article.content)
+	    end
+	  end
+
+      describe "#sort_by_field" do
+  
+        it "should have ascending order in id field" do
+        	click_on 'Id'
+        	within "#article_#{article.id}" do
+	    	 expect(page).to have_content(article.id)
+	    	end
+	    	within "#article_#{article2.id}" do
+	    	 expect(page).to have_content(article2.id)
+	    	end
+        end
+
+        it "should have descending order in id field" do
+        	click_on 'Id'
+        	click_on 'Id'
+        	within "#article_#{article2.id}" do
+	    	 expect(page).to have_content(article2.id)
+	    	end
+	    	within "#article_#{article.id}" do
+	    	 expect(page).to have_content(article.id)
+	    	end
+        end
+      end
     end
+
 
 end
